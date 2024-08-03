bbb5992035214a2dd10a999f471d319c16bc2849
Kaushik035 <kaushikmandal629@gmail.com>
2024-08-04 00:56:05 +0530

    Cursor  animation

--- asmp/src/components/UltimateHome/Homee.jsx
+++ asmp/src/components/UltimateHome/Homee.jsx
@@ -3,14 +3,18 @@ import About from "../About/About"
 import Home from "../Home/Home"
 import Faq from "../Faq"
 import TestimonialSlider from "../TestimonialSlider/Testimonial"
+import CursorAnimation from '../CursorAnimation'
 
 export default function Homee(params) {
 
     return(
         <>
-        <Home />
+        <CursorAnimation />
+      <Home />
       <About/>
+      <div id="testimonials"></div>
       <TestimonialSlider></TestimonialSlider>
+      <div id="faq"></div>
       <Faq />
         </>
     )
