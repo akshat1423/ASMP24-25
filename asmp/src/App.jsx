4168ccf7c82bfd369504f3537ef0d974cbe19a75
redcoder-09 <yashkrishnani09@gmail.com>
2024-08-03 16:36:31 +0530

    Yash Login/Signup Components

--- asmp/src/App.jsx
+++ asmp/src/App.jsx
@@ -1,12 +1,22 @@
-
-import './App.css'
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+import Register from './components/register.jsx';
+import Login from './components/login.jsx';
 
 function App() {
 
   return (
-    <>
-      <div className="text-primary">ASMP</div>
-    </>
+    <Router>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>
   )
 }
 
