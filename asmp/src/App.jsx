04ddf0f0270cf5dd761a0bce01c30024c2ceba4d
pratiksha <pratikshajjn045@gmail.com>
2024-08-04 13:01:12 +0530

    done

--- asmp/src/App.jsx
+++ asmp/src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Sneakpeak from './components/sneakpeak/sneakpeak';
+import EventImages from './components/events/events';
 
 function App() {
-
   return (
-    <>
+    <div className="App">
       <div className="text-primary">ASMP</div>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Sneakpeak />} />
+        <Route path='/events' element={<EventImages/>}/>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
